8fdfa3e41519cee574841573fa96ddd1273f0d8d
Andrey Rabchevskiy <longmayar@gmail.com>
2017-08-15 20:18:06 +0300

    TASK_ALL Fixes

--- Tasks/Tests/TheBox/Task2Test.php
+++ Tasks/Tests/TheBox/Task2Test.php
@@ -11,7 +11,6 @@ final class Task2Test extends Task2
     protected function getContainer(): ContainerInterface
     {
         $container = new ContainerBuilder();
-        $container->register('service_container', 'Symfony\Component\DependencyInjection\ContainerBuilder');
         $container->compile();
 
         return $container;
